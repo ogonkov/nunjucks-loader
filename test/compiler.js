82048527e38a172a2dc917d323285b7432c457a9
Alexey <lesha.ogonkov@gmail.com>
2019-07-14 23:18:09 +0300

    Add basic tests

---
+++ test/compiler.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import webpack from 'webpack';
+import uuidv4 from 'uuid/v4';
+
+export default (fixture, options = {}) => {
+    const bundleName = uuidv4();
+
+    const compiler = webpack({
+        mode: 'development',
+        devtool: false,
+        target: 'node',
+        context: __dirname,
+        entry: `./${fixture}`,
+        output: {
+            libraryTarget: 'commonjs2',
+            path: path.resolve(__dirname),
+            filename: `bundles/${bundleName}.js`,
+        },
+        module: {
+            rules: [{
+                test: /\.njk$/,
+                use: {
+                    loader: path.resolve(__dirname, '../src/loader.js'),
+                    options: options
+                }
+            }]
+        }
+    });
+
+    return new Promise((resolve, reject) => {
+        compiler.run((err, stats) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (stats.hasErrors()) {
+                reject(new Error(stats.toJson().errors));
+            }
+
+
+
+            resolve(require(`./bundles/${bundleName}.js`));
+        });
+    });
+}
---
+++ test/loader.test.js
@@ -0,0 +1,21 @@
+import compiler from './compiler';
+
+describe('Simple compilation', function() {
+    test('should compile single files', async function() {
+        const output = await compiler('fixtures/single.njk');
+
+        expect(output({
+            title: 'App',
+            href: 'https://example.com'
+        })).toMatchSnapshot();
+    });
+
+    test('should compile templates with inheritance', async function() {
+        const output = await compiler('fixtures/child.njk');
+
+        expect(output({
+            parent_link: 'https://example.com/1',
+            child_link: 'https://example.com/2'
+        })).toMatchSnapshot();
+    });
+});
