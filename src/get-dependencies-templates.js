d199f312b5fffcf280d5e0e5e2f8cdbfdfbb751f
Alexey <lesha.ogonkov@gmail.com>
2019-08-20 22:21:46 +0300

    Refactor globals dependencies

--- src/get-dependencies-templates.js
+++ src/get-dependencies-templates.js
@@ -1,7 +1,6 @@
 import nunjucks from 'nunjucks';
 
-export function getDependencies(source) {
-    const nodes = nunjucks.parser.parse(source);
+export function getDependenciesTemplates(nodes) {
     const extendsNodes = nodes.findAll(nunjucks.nodes.Extends);
     const includeNodes = nodes.findAll(nunjucks.nodes.Include);
     const importNodes = nodes.findAll(nunjucks.nodes.Import);
