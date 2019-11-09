b1db7122b03cedeeaa4c5d42599a670839d497a1
Alexey <lesha.ogonkov@gmail.com>
2019-11-09 14:40:17 +0300

    Refactor `getImportPath`

--- src/get-import-path.js
+++ src/get-import-path.js
@@ -1,8 +1,13 @@
 import path from 'path';
 
+/**
+ * @param {string}   resourcePath
+ * @param {string[]} searchPaths
+ * @returns {string}
+ */
 export function getImportPath(resourcePath, searchPaths) {
-    const [importPath] = searchPaths.map((x) => resourcePath
-            .replace(path.resolve(x), '')
+    const [importPath] = searchPaths.map((searchPath) => resourcePath
+            .replace(path.resolve(searchPath), '')
             .replace(/^\//, '')
         ).sort(function(a, b) {
             return a.length - b.length;
