27ecbddddfa824a3cd386bb0341a5f6dcbcc90f2
Alexey <lesha.ogonkov@gmail.com>
2019-12-03 23:41:15 +0300

    Move coverage folder (#23)

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'test/.coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
