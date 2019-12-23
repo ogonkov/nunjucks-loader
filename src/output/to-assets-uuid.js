501d8115d618cea64a6a54b619233ab726b8e1d4
Alexey <lesha.ogonkov@gmail.com>
2019-12-23 22:21:13 +0300

    Replace `uuid` with homemade string hashing to avoid redundant dependency

--- src/output/to-assets-uuid.js
+++ src/output/to-assets-uuid.js
@@ -1,9 +1,9 @@
-import uuid from 'uuid/v5';
+import {getStringBas64Hash} from './get-string-base64-hash';
 
 export function toAssetsUUID(assets) {
     return assets.map(function(assetPath) {
         return [
-            uuid(assetPath, uuid.URL),
+            getStringBas64Hash(assetPath),
             assetPath
         ];
     })
