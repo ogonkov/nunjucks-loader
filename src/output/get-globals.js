5f3e639e786e287ad97d85856c13f43e3ce539bd
Alexey <lesha.ogonkov@gmail.com>
2020-01-30 22:25:10 +0300

    Debug inlining

--- src/output/get-globals.js
+++ src/output/get-globals.js
@@ -4,9 +4,9 @@ export function getGlobals(globals) {
     console.log('getGlobals', globals[0]);
 
     function imports() {
-        return globals.map(([globalImport, globalPath]) => (
+        return globals.map(([globalImport, globalPath]) => console.log(JSON.stringify(globalPath)) || (
             `
-                var _global_${toVar(globalImport)} = require('${globalPath}');
+                var _global_${toVar(globalImport)} = require(${JSON.stringify(globalPath)});
                 __nunjucks_module_dependencies__.globals['${globalImport}'] = {
                     module: _global_${toVar(globalImport)}
                 };
