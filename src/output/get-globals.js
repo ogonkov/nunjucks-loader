ad45da1e2ffe0e8d25a5e4090a566a8808d0afea
Alexey <lesha.ogonkov@gmail.com>
2020-01-30 22:31:46 +0300

    Revert console.log

--- src/output/get-globals.js
+++ src/output/get-globals.js
@@ -1,10 +1,8 @@
 import {toVar} from '../to-var';
 
 export function getGlobals(globals) {
-    console.log('getGlobals', globals[0]);
-
     function imports() {
-        return globals.map(([globalImport, globalPath]) => console.log(JSON.stringify(globalPath)) || (
+        return globals.map(([globalImport, globalPath]) => (
             `
                 var _global_${toVar(globalImport)} = require(${JSON.stringify(globalPath)});
                 __nunjucks_module_dependencies__.globals['${globalImport}'] = {
