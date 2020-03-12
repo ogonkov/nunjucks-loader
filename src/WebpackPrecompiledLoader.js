b7ff92958d096f4489f2581bceb462814f362146
Alexey <lesha.ogonkov@gmail.com>
2020-03-12 23:24:18 +0300

    Change template name normalization

--- src/WebpackPrecompiledLoader.js
+++ src/WebpackPrecompiledLoader.js
@@ -5,18 +5,16 @@ export function WebpackPrecompiledLoader(precompiled = {}, {isWindows} = {}) {
 
 WebpackPrecompiledLoader.prototype.getSource = function getSource(name) {
     // For some strange reason all precompiled templates get leading slashes
-    // on Windows, while templates have it without it
-    const windowsIdentifier = `/${name}`;
-    console.log(this.isWindows, name, Object.keys(this.precompiled));
-    if (!this.isWindows && !(name in this.precompiled) ||
-        this.isWindows && !(windowsIdentifier in this.precompiled)) {
+    // on Windows, while templates sometimes have it without it
+    const id = this.isWindows && !name.startsWith('/') ? `/${name}` : name;
+    if (!(id in this.precompiled)) {
         return null;
     }
 
     return {
         src: {
             type: 'code',
-            obj: this.precompiled[name] || this.isWindows && this.precompiled[windowsIdentifier]
+            obj: this.precompiled[id]
         },
         path: name
     };
