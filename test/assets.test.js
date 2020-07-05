00590d24624ddf25070124163a39d672dfc01293
Alexey <lesha.ogonkov@gmail.com>
2020-07-05 10:36:38 +0300

    Add exported value tests

--- test/assets.test.js
+++ test/assets.test.js
@@ -1,22 +1,32 @@
 import compiler from './compiler';
 
+const loaderOptions = {
+    assetsPaths: [
+        'test/fixtures/django_project/app_example/static'
+    ]
+};
+
 describe('Assets', function() {
     test('should load static assets', async function() {
-        const output = await compiler('fixtures/assets/template.njk', {
-            assetsPaths: [
-                'test/fixtures/django_project/app_example/static'
-            ]
-        });
+        const output = await compiler('fixtures/assets/template.njk', loaderOptions);
+
+        await expect(output()).resolves.toMatchSnapshot();
+    });
+
+    test('should load exported static assets', async function() {
+        const output = await compiler('fixtures/assets/template-as.njk', loaderOptions);
 
         await expect(output()).resolves.toMatchSnapshot();
     });
 
     test('should load dynamic assets', async function() {
-        const output = await compiler('fixtures/assets/dynamic.njk', {
-            assetsPaths: [
-                'test/fixtures/django_project/app_example/static'
-            ]
-        });
+        const output = await compiler('fixtures/assets/dynamic.njk', loaderOptions);
+
+        await expect(output()).resolves.toMatchSnapshot();
+    });
+
+    test('should load exported dynamic assets', async function() {
+        const output = await compiler('fixtures/assets/dynamic-as.njk', loaderOptions);
 
         await expect(output()).resolves.toMatchSnapshot();
     });
