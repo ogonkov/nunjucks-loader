a5214cf95c4290bfe6a6617258dc115a599d6adc
Alexey <lesha.ogonkov@gmail.com>
2020-07-08 22:40:55 +0300

    Add esm build

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function(api) {
     return {
         presets: [
             ['@babel/preset-env', {
+                modules: isESM ? false : 'auto',
                 targets: {
                     node: isESM ? '13.2.0' : '8.9.0'
                 }
