e4ebf8dbfe82fec5e38eb9ab0a951bf5a5fe5e0c
Alexey <lesha.ogonkov@gmail.com>
2020-11-07 16:55:04 +0300

    Convert `getFirstExistedPath` to `async` function

--- src/utils/get-first-existed-path.js
+++ src/utils/get-first-existed-path.js
@@ -1,11 +1,9 @@
 import fs from 'fs';
 import {promisify} from 'util';
 
-import {ERROR_MODULE_NOT_FOUND} from '../constants';
-
-import {getErrorCopy} from './get-error-copy';
 import {getGlob} from './get-glob';
 import {unquote} from './unquote';
+
 
 const fsAccess = promisify(fs.access);
 
@@ -37,29 +35,13 @@ function getGlobExpression(pathExpression) {
     return unquote(pathExpression.replace(/" \+ [^ ]+( \+ "|$)/g, '*'));
 }
 
-export function getFirstExistedPath(paths) {
-    return paths.reduce(function(resolved, path) {
-        return resolved.then(function(existedFile) {
-            if (typeof existedFile === 'string') {
-                return existedFile;
-            }
-
-            return isExists(path).then(
-                () => path,
-                (error) => {
-                    if (error.code !== ERROR_MODULE_NOT_FOUND) {
-                        return false;
-                    }
-
-                    throw getErrorCopy(error);
-                }
-            );
-        });
-    }, Promise.resolve()).then(function(path) {
-        if (typeof path !== 'string') {
-            throw new Error('Template not found');
+export async function getFirstExistedPath(paths) {
+    for (const path of paths) {
+        const exist = await isExists(path);
+        if (exist) {
+            return path;
         }
+    }
 
-        return path;
-    })
+    throw new Error('Path not found');
 }
