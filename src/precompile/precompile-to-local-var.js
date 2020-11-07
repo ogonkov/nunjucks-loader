5614c7b5e4e5ebdc7017d6797cc142b61db383be
Alexey <lesha.ogonkov@gmail.com>
2020-11-07 18:29:55 +0300

    Update JSDoc

--- src/precompile/precompile-to-local-var.js
+++ src/precompile/precompile-to-local-var.js
@@ -7,7 +7,7 @@ import {localVarWrapper as precompileWrapper} from './local-var-wrapper';
  * @param {string}      source
  * @param {string}      fileName
  * @param {nunjucks.Environment} env
- * @returns {string}
+ * @returns {string} Precompiled template, assigned to given `fileName`
  */
 export function precompileToLocalVar(source, fileName, env) {
     return nunjucks.precompileString(source, {
