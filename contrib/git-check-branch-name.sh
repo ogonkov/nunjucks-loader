4b7f203973cd9d22ded4dcdedcf7c68ddf148244
Alexey <lesha.ogonkov@gmail.com>
2020-11-14 17:58:34 +0300

    Extract `git-utils.sh`

--- contrib/git-check-branch-name.sh
+++ contrib/git-check-branch-name.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(dirname -- "$0")"
+# shellcheck source=git-utils.sh
+. "$SCRIPT_DIR/git-utils.sh"
+
+is_git_repo
+
 CHECK_TARGET_BRANCH_NAME=${1:-master}
-
-if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-  echo "Not a git repo."
-  exit 1;
-fi
-
 CHECK_CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if [ "$CHECK_TARGET_BRANCH_NAME" != "$CHECK_CURRENT_BRANCH" ]; then
