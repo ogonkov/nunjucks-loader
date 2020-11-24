774baad369bc3946d9827dd573f27aafd85ecb27
Alexey <lesha.ogonkov@gmail.com>
2020-11-24 23:33:58 +0300

    Fix remote branch hash check (#104)

--- contrib/git-check-branch-is-even.sh
+++ contrib/git-check-branch-is-even.sh
@@ -11,9 +11,18 @@ is_git_repo
 git fetch origin
 
 CHECK_GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
+CHECK_GIT_BRANCH_HASH="$(git rev-parse "$CHECK_GIT_BRANCH_NAME")"
+
 CHECK_GIT_REMOTE_BRANCH_NAME="$(git status -b --porcelain=v2 | grep -m 1 "^# branch.upstream " | cut -d " " -f 3-)"
 
-if [ "$CHECK_GIT_BRANCH_NAME" != "$CHECK_GIT_REMOTE_BRANCH_NAME" ]; then
+if [ "$CHECK_GIT_REMOTE_BRANCH_NAME" = '' ]; then
+  echo "Branch didn't have remote."
+  exit 1;
+fi
+
+CHECK_GIT_REMOTE_BRANCH_HASH="$(git rev-parse "$CHECK_GIT_REMOTE_BRANCH_NAME")"
+
+if [ "$CHECK_GIT_BRANCH_HASH" != "$CHECK_GIT_REMOTE_BRANCH_HASH" ]; then
   echo "Branch is not even with it origin"
   exit 2;
 fi
