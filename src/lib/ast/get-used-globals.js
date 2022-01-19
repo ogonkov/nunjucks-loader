e3a9bcdca3a42c91f38168a93498a538e11b82a5
Alexey <lesha.ogonkov@gmail.com>
2022-01-19 13:27:18 +0300

    Refactor `getUsedGlobals`

--- src/lib/ast/get-used-globals.js
+++ src/lib/ast/get-used-globals.js
@@ -2,6 +2,23 @@ import nunjucks from 'nunjucks';
 
 import {getUsagesOf} from './get-usages-of';
 
+
+/**
+ * @param {nunjucks.nodes.FunCall} functionNode
+ * @returns {function(AddonWrapper): boolean}
+ */
+function getFunctionNodeMatcher(functionNode) {
+    /**
+     * @param {AddonWrapper} addon
+     * @returns {boolean}
+     */
+    function isSameFunction(addon) {
+        return functionNode.name.value === addon.name;
+    }
+
+    return isSameFunction;
+}
+
 /**
  * Filter list of globals
  *
@@ -11,8 +28,7 @@ import {getUsagesOf} from './get-usages-of';
  */
 export function getUsedGlobals(nodes, globals) {
     return getUsagesOf(nunjucks.nodes.FunCall, nodes)(
-        globals, ({name: globalName}) => ({name}) => (
-            globalName.value === name
-        )
+        globals,
+        getFunctionNodeMatcher
     );
 }
