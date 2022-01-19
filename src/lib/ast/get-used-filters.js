d633cbad12ea29ea12245692133b9561ceb5fea8
Alexey <lesha.ogonkov@gmail.com>
2022-01-19 13:36:56 +0300

    Refactor `getUsedFilters`

--- src/lib/ast/get-used-filters.js
+++ src/lib/ast/get-used-filters.js
@@ -2,6 +2,18 @@ import nunjucks from 'nunjucks';
 
 import {getUsagesOf} from './get-usages-of';
 
+function getFilterNodeMatcher(filterNode) {
+    /**
+     * @param {AddonWrapper} addon
+     * @returns {boolean}
+     */
+    function isSameNode(addon) {
+        return addon.name === filterNode.name.value;
+    }
+
+    return isSameNode;
+}
+
 /**
  * @param {nunjucks.nodes.Root} nodes
  * @param {AddonWrapper[]}      instances
@@ -9,8 +21,7 @@ import {getUsagesOf} from './get-usages-of';
  */
 export function getUsedFilters(nodes, instances) {
     return getUsagesOf(nunjucks.nodes.Filter, nodes)(
-        instances, ({name}) => (
-            ({name: filterName}) => filterName === name.value
-        )
+        instances,
+        getFilterNodeMatcher
     );
 }
