93613ef0a14eab47bd23ac13c896ed41d61636c5
Alexey <lesha.ogonkov@gmail.com>
2022-01-19 14:12:17 +0300

    Add JSDoc

--- src/lib/utils/is-unique-object.js
+++ src/lib/utils/is-unique-object.js
@@ -1,5 +1,19 @@
+/**
+ * @template TItem
+ * @param {function(TItem[], TItem): number} getIndex
+ * @returns {function(TItem, number, TItem[]): boolean}
+ */
 export function isUniqueObject(getIndex) {
-    return function compareIndexes(item, index, list) {
+    /**
+     * @template TItem
+     * @param {TItem} item
+     * @param {number} index
+     * @param {TItem[]} list
+     * @returns {boolean}
+     */
+    function compareIndexes(item, index, list) {
         return getIndex(list, item) === index;
     }
+
+    return compareIndexes;
 }
